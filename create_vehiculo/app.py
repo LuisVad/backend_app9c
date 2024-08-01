3bc5aeeedf1b13e6c159becbd0f57c52bf9c4f20
Luis Valladolid <105667043+LuisVad@users.noreply.github.com>
2024-08-01 16:30:13 -0600

    [UPDATE] app.py CREATE

--- create_vehiculo/app.py
+++ create_vehiculo/app.py
@@ -2,35 +2,31 @@ import json
 import os
 import boto3
 from botocore.exceptions import ClientError
+from datetime import datetime
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
-    id = event['pathParameters']['id']
     body = json.loads(event['body'])
-    update_expression = "SET marca = :marca, modelo = :modelo, autonomia = :autonomia, velocidadMaxima = :velocidadMaxima, dueno = :dueno, caballosDeFuerza = :caballosDeFuerza"
-    expression_values = {
-        ':marca': body['marca'],
-        ':modelo': body['modelo'],
-        ':autonomia': body['autonomia'],
-        ':velocidadMaxima': body['velocidadMaxima'],
-        ':dueno': body['dueño'],  # Cambiado a 'dueno'
-        ':caballosDeFuerza': body['caballosDeFuerza']
+    item = {
+        'id': body['id'],
+        'marca': body['marca'],
+        'modelo': body['modelo'],
+        'autonomia': body['autonomia'],
+        'velocidadMaxima': body['velocidadMaxima'],
+        'dueño': body['dueño'],  # Agregado
+        'caballosDeFuerza': body['caballosDeFuerza']  # Agregado
     }
 
     try:
-        table.update_item(
-            Key={'id': id},
-            UpdateExpression=update_expression,
-            ExpressionAttributeValues=expression_values
-        )
+        table.put_item(Item=item)
         return {
             'statusCode': 200,
-            'body': json.dumps({'message': 'Vehículo actualizado exitosamente'})
+            'body': json.dumps({'message': 'Vehículo creado exitosamente'})
         }
     except ClientError as e:
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': 'Error updating item', 'details': str(e)})
+            'body': json.dumps({'error': str(e)})
         }
